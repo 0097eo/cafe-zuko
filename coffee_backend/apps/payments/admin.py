06c4968f9dfc3a8b61add70c1af9734952b291fd
0097eo <emmanuelokello294@gmail.com>
2025-01-13 10:19:15 +0300

    Register models in admin for accounts, orders, payments, and products with custom admin configurations

--- coffee_backend/apps/payments/admin.py
+++ coffee_backend/apps/payments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Payment, Refund
 
 # Register your models here.
+admin.site.register(Payment)
+admin.site.register(Refund)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('transaction_id', 'amount', 'payment_method', 'status', 'created_at', 'updated_at')
+    search_fields = ('transaction_id', 'amount', 'payment_method', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'created_at', 'updated_at')
--- coffee_backend/apps/products/admin.py
+++ coffee_backend/apps/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from.models import Category, Product, ProductReview
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductReview)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'vendor', 'category', 'price', 'stock', 'roast_type', 'origin', 'is_available', 'created_at', 'updated_at')
+    search_fields = ('name', 'vendor', 'category', 'price', 'stock', 'roast_type', 'origin', 'is_available', 'created_at', 'updated_at')
+    list_filter = ('vendor', 'category', 'roast_type', 'is_available', 'created_at', 'updated_at')
+
