9620de4b2c3d2ec5742a486c6acece7db2913b19
0097eo <emmanuelokello294@gmail.com>
2025-01-16 11:03:55 +0300

    Add routing for Signup, Login, Products, About, Contact, and User Dashboard pages

--- coffee_frontend/src/App.jsx
+++ coffee_frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import NavBar from "./components/NavBar"
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from "./pages/LandingPage"
+import SignupPage from "./pages/Signup"
+import LoginPage from "./pages/Login"
+import Footer from "./components/Footer"
+import ProductsPage from "./pages/Products"
+import AboutPage from "./pages/About"
+import ContactPage from "./pages/Contacts"
+import UserDashboard from "./pages/Dashboard"
+
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/shop" element={<ProductsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/profile" element={<UserDashboard />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
